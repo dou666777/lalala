f5d5336acae796e933470e053f2031af37ce568a
dou666777@126.com <lala@163.com>
2017-08-27 21:52:37 +0800

    lalala

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,8 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.print("sss");
+        for (int i = 0;i < 2; i++){
+            System.out.print("sss")
+        }
     }
 }
